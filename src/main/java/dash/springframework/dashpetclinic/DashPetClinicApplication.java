b490340bddb960bc3aeb88426f25d8f55a4ee70c
Dhiren Dash <dashdhirens@gmail.com>
2021-04-17 01:58:55 +0530

    Initial commit spring boot application

---
+++ src/main/java/dash/springframework/dashpetclinic/DashPetClinicApplication.java
@@ -0,0 +1,13 @@
+package dash.springframework.dashpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dash/springframework/dashpetclinic/DashPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dash.springframework.dashpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
