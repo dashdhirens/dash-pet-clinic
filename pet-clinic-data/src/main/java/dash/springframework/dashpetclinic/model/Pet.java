d47e893bf2ae2c6b5b7a3bb02380f4f22a8db399
Dhiren Dash <dhiren.dash@expeditors.com>
2021-04-27 18:43:19 +0530

    Add base entity for ID.

--- pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package dash.springframework.dashpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
--- pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package dash.springframework.dashpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
