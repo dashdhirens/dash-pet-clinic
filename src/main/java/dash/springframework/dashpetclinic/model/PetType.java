4669f223bffc7038b0ff8e5ba9d4b25189dbe426
Dhiren Dash <dhiren.dash@expeditors.com>
2021-04-27 12:15:21 +0530

    Initial POJOs commit

---
+++ src/main/java/dash/springframework/dashpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package dash.springframework.dashpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/dash/springframework/dashpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package dash.springframework.dashpetclinic.model;
+
+public class Vet extends Person {
+}
