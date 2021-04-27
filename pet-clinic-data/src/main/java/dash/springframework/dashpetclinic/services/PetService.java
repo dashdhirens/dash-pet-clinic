40ac414a244ecba72b12902fd2f763b864780338
Dhiren Dash <dhiren.dash@expeditors.com>
2021-04-27 18:30:30 +0530

    Inital commit of service interfaces

---
+++ pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package dash.springframework.dashpetclinic.services;
+
+import dash.springframework.dashpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package dash.springframework.dashpetclinic.services;
+
+import dash.springframework.dashpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
