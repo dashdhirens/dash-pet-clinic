18dd6ed667bf2faa1c55d041bd991fb9d3f876f8
Dhiren Dash <dhiren.dash@expeditors.com>
2021-04-28 21:12:32 +0530

    Refactor crud base methods into an interface.

---
+++ pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package dash.springframework.dashpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    T findById(ID id);
+
+    Set<T> findAll();
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
