24d46545098206d7e150b29e9a1a97cfe4fa3630
Dhiren Dash <dhiren.dash@expeditors.com>
2021-04-29 16:36:44 +0530

    Add initial UI pages and controllers for Vets, Pets and Owners

---
+++ pet-clinic-data/src/main/java/dash/springframework/dashpetclinic/controllers/VetsController.java
@@ -0,0 +1,14 @@
+package dash.springframework.dashpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetsController {
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
